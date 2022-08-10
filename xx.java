ea28f0a929ec31035f233320937d0e5e30999ec2
govardhan151 <110959555+govardhan151@users.noreply.github.com>
2022-08-10 17:13:50 +0530

    Add files via upload

---
+++ xx.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+class xx
+{
+ public static void main(String[] args)
+{
+  Scanner sc=new Scanner(System.in);
+  int a,b;
+    a= sc.nextInt();
+b=a+10;
+System.out.println(b);
+}
+}
